5aaa81de4e76c404fcb323c355d69a72827094a0
futoid <aliekmandal24@gmail.com>
2022-11-23 15:12:31 +0530

    server created

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+require('dotenv').config()
+
+const app = express();
+
+app.listen( process.env.PORT|| 4000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Server Started Successfully.");
+  }
+});
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+app.use(express.json());
+app.use("/", authRoutes);
